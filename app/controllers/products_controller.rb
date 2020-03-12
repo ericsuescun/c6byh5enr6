14df109f8cc567fdcfdbd264749b8aea28741cc9
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-03-11 23:37:41 -0500

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,32 +1,43 @@
-class ProductsController < ApplicationController
-  
-  def api
-    products = Product.all
-    render json: products, status: 200
-  end
-
-  def index
-    @products = Product.all
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.create(product_params)
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.update(params[:id], product_params)
-  end
-
-  private
-    def product_params
-      params.require(:product).permit(:name, :price)
+module Api
+  module V1
+    class ProductsController < ApplicationController
+      respond_to :json
+      def index
+        respond_with Product.all
+      end
+      # def create
+      #   user = User.new(user_params)
+      #   if user.save
+      #     render json: user, status: 201, location: [:api, user]
+      #   else
+      #     render json: { errors: user.errors }, status: 422
+      #   end
+      # end
+      def create
+        product = Product.new(product_params)
+        if product.save
+          render json: product, status: 201
+        else
+          render json: { errors: product.errors }, status: 422
+        end
+      end
+      def update
+        product = Product.find(params[:id])
+        if product.update(product_params)
+          render json: product, status: 200
+        else
+          render json: { errors: product.errors }, status: 422
+        end
+      end
+      def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        head 204
+      end
+      private
+        def product_params
+          params.require(:product).permit(:name, :price)
+        end
     end
+  end
 end
